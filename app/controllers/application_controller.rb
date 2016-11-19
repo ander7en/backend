e840b8b6ae909b3527d914af1d2ee4321d338f51
Andrii Khrystian <andrii.khrystian@gmail.com>
2016-11-19 19:40:12 +0200

    initial version of booking controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::API
-protect_from_forgery with: :null_session
+  include ActionController::RequestForgeryProtection
+  protect_from_forgery with: :null_session
+
+  # CSRF is turned off
+  skip_before_action :verify_authenticity_token
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: redirect('/booking')
+  get '/booking', to: 'booking#index'
+  post '/booking', to: 'booking#create'
 end
