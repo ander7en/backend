ca003c51584907820d03071a594aa0bc3a57beb4
Giorgi Topuria <gtopuria2@gmail.com>
2016-11-20 19:51:12 +0200

    spelling error fix, getting drivers list sorted by distance (#7)
    * initial version of booking controller
    
    * csrf is suppose to be working with angular front end now
    
    * csrf support fixed
    
    * adding simple serving controller and orders model
    
    * Add db reset in travis
    
    * Remove db:test:prepare
    
    * test if statement travce
    
    * travice if in service
    
    * calculating closest drivers, spelling error fix

--- spec/factories/order.rb
+++ spec/factories/order.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :order do
-    source_longtitude 26.732116
+    source_longitude 26.732116
     source_latitude 58.382482
-    dest_longtitude 26.715456
+    dest_longitude 26.715456
     dest_latitude 58.378217
     status 1
     serving_driver 12
