87238a511212baf39b79deea7dae0c2d8ee4644d
Andrii Khrystian <andrii.khrystian@gmail.com>
2016-11-20 13:42:31 +0200

    csrf is suppose to be working with angular front end now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::API
   include ActionController::RequestForgeryProtection
   protect_from_forgery with: :null_session
+  after_filter :set_csrf_cookie_for_ng
 
+  def verified_request?
+    super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
+  end
+
+  def set_csrf_cookie_for_ng
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
   # CSRF is turned off
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 end
