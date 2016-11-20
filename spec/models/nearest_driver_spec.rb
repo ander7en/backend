039e21d4a15a140aa62879bbac31c099059a1210
Demur Nodia <demur.nodia@gmail.com>
2016-11-21 01:56:15 +0200

    Remove depreciatin warnings

--- spec/models/nearest_driver_spec.rb
+++ spec/models/nearest_driver_spec.rb
@@ -5,21 +5,21 @@ describe 'return sorted drivers by distance' do
     @originLocation = {longitude: 26.7124793, latitude: 58.3782485}
   end
 
-  it "should calculate and sort drivers by distance" do
-    driverKaubamaja = Driver.new
-    driverKaubamaja.firstName = 'kaubamajaDriver'
-    driverKaubamaja.longitude = 26.7257195
-    driverKaubamaja.latitude = 58.3777226
+  it 'should calculate and sort drivers by distance' do
+    driver_kaubamaja = Driver.new
+    driver_kaubamaja.firstName = 'kaubamajaDriver'
+    driver_kaubamaja.longitude = 26.7257195
+    driver_kaubamaja.latitude = 58.3777226
 
-    driverLounakeskus = Driver.new
-    driverLounakeskus.firstName = 'lonakeskusDriver'
-    driverLounakeskus.longitude = 26.6754045
-    driverLounakeskus.latitude = 58.3570084
+    driver_lounakeskus = Driver.new
+    driver_lounakeskus.firstName = 'lonakeskusDriver'
+    driver_lounakeskus.longitude = 26.6754045
+    driver_lounakeskus.latitude = 58.3570084
 
 
-    sortedDriversByDistance = NearestDriver.getNearestDrivers(@originLocation , [driverLounakeskus , driverKaubamaja])
+    sorted_drivers_by_distance = NearestDriver.getNearestDrivers(@originLocation, [driver_lounakeskus, driver_kaubamaja])
 
-    sortedDriversByDistance.should eq [driverKaubamaja , driverLounakeskus]
+    expect(sorted_drivers_by_distance).to eq([driver_kaubamaja, driver_lounakeskus])
 
   end
 
