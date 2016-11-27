e41aed274e44e27d6bc53fe53fa14b6447c327e0
Andrii Khrystian <andrii.khrystian@gmail.com>
2016-11-27 15:49:43 +0200

    Initial step for autodeploy to heroku

--- app/jobs/driver_query_job.rb
+++ app/jobs/driver_query_job.rb
@@ -2,6 +2,7 @@ class DriverQueryJob
   include SuckerPunch::Job
   
   def perform (driverList)
+    # for the purpose of initial implementation
     driverList[0]
   end
 end
--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,4 +1,4 @@
 class Driver < ApplicationRecord
   enum status: [:free, :busy]
-  has_many :orders, foreign_key: "serving_driver", dependent: :restrict_with_exception
+  has_many :orders, foreign_key: 'driver_id', dependent: :restrict_with_exception
 end
