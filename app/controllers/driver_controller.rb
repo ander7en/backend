feb696a2f4e87d00e04b33eb3ba040af4f6c46ca
Demur Nodia <demur.nodia@gmail.com>
2016-12-10 21:58:57 +0200

    Driver backend impm

--- app/controllers/driver_controller.rb
+++ app/controllers/driver_controller.rb
@@ -3,9 +3,18 @@ class DriverController < ApplicationController
   def nearby_drivers
 
     lng = params[:lng].to_f
-    ltd = params[:lat].to_f
+    ltd = params[:ltd].to_f
+
+    if lng == 0.0 or ltd == 0.0
+      raise 'Bad Request'
+    end
 
     nearby_drivers = DriverUtility.get_nearby_drivers({ :lng => lng, :lat => ltd})
+
+    # TODO ADD ENV CHECK IF IN DEMO MODE ONLY
+    if nearby_drivers.empty?
+      nearby_drivers = DriverUtility.generate_drivers({:lng => lng, :lat => ltd}, 10, 2000)
+    end
 
     drivers = []
 
