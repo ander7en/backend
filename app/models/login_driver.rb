962cc3ef4415bf1dcd75ca28934925c83fa884cc
gtopuria <gtopuria2@gmail.com>
2016-12-10 17:13:36 +0200

    adding driver channels table

--- app/models/login_driver.rb
+++ app/models/login_driver.rb
@@ -1,10 +1,11 @@
 class LoginDriver
 
-  def self.login(email, password)
+  def self.login(email, password, driverId)
 
     driver = Driver.where(email: email, password: password).take
 
     if !driver.nil?
+
         return 'Success'
     else
       error = 'incorrect username or password'
