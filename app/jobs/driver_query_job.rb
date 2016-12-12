20bdb1d1e6efecc14d388ef64db24c090bda240a
gtopuria <gtopuria2@gmail.com>
2016-12-12 22:43:04 +0200

    driver qurery job fix

--- app/jobs/driver_query_job.rb
+++ app/jobs/driver_query_job.rb
@@ -4,21 +4,23 @@ class DriverQueryJob
   #take response from notification
 
   def self.perform (driverList, order)
-
     driverList.each do |t|
-      channel = DriverChannel.where(driver_id: t.id)
-      if !channel.channel_id.nil?
+      channel = DriverChannel.where(driver_id: t.id).take
+      if !channel.nil? && !channel.channel_id.nil?
         channel_id = channel.channel_id
 
-        Pusher.trigger(channel_id + '_channel', 'notify', {
-            OrderInfo: {slat: order.source_latitude, slong: order.source_longitude,
-                        tlat: order.dest_latitude, tlong: order.source.longitude}
-        })
+        # Pusher.trigger(channel_id + '_channel', 'notify', {
+        #     OrderInfo: {slat: order.source_latitude, slong: order.source_longitude,
+        #                 tlat: order.dest_latitude, tlong: order.source.longitude}
+        # })
+
         #get respone from pusher
         response = true;
         if response
             return t
         end
+      else
+        return driverList[0]
       end
      end
   end
