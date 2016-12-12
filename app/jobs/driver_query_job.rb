d89d9e28ce6096f321a589fcbfb06931c7c0bedc
gtopuria <gtopuria2@gmail.com>
2016-12-12 23:10:47 +0200

    fix tests

--- app/jobs/driver_query_job.rb
+++ app/jobs/driver_query_job.rb
@@ -8,12 +8,12 @@ class DriverQueryJob
       channel = DriverChannel.where(driver_id: t.id).take
       if !channel.nil? && !channel.channel_id.nil?
         channel_id = channel.channel_id
-
         # Pusher.trigger(channel_id + '_channel', 'notify', {
         #     OrderInfo: {slat: order.source_latitude, slong: order.source_longitude,
         #                 tlat: order.dest_latitude, tlong: order.source.longitude}
         # })
         #get respone from pusher
+        
         response = true;
         if response
             return t
