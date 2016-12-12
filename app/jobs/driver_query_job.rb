69c35e8aa0b9b2c5ba1031898a3fb459e2f075d3
gtopuria <gtopuria2@gmail.com>
2016-12-12 20:46:40 +0200

    getting channel id/trigger pusher

--- app/jobs/driver_query_job.rb
+++ app/jobs/driver_query_job.rb
@@ -3,17 +3,26 @@ class DriverQueryJob
   #make pusher notification to driver
   #take response from notification
 
-  def self.perform (driverList)
+  def self.perform (driverList, order)
 
     driverList.each do |t|
+      channel = DriverChannel.where(driver_id: t.id)
+      if !channel.channel_id.nil?
+        channel_id = channel.channel_id
 
-      if !driver.channel_id.nil?
+        Pusher.trigger(channel_id + '_channel', 'notify', {
+            OrderInfo: {slat: order.source_latitude, slong: order.source_longitude,
+                        tlat: order.dest_latitude, tlong: order.source.longitude}
+        })
+        #get respone from pusher
+        response = true;
+        if response
+            return t
+        end
 
-      end  
+      end
 
     end
-
-    driverList[0]
-    #return Driver.where(id: driver_ids[0]).take
+    #driverList[0]
   end
 end
