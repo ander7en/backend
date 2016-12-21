7117a890ac17c5a67999b6a44aa09672603d2be5
Andrii Khrystian <andrii.khrystian@gmail.com>
2016-12-21 21:20:02 +0200

    These test are no longer required

--- spec/controllers/driver_controller_spec.rb
+++ spec/controllers/driver_controller_spec.rb
@@ -11,25 +11,25 @@ RSpec.describe DriverController, type: :controller do
       DriverUtility.generate_drivers(@location, @number_of_drivers, @radius)
     end
 
-    it 'returns json object' do
-      get :nearby_drivers, params: {lng: 58.382856, lat: 26.732627}
-      expect(response.content_type).to eq 'application/json'
-    end
+    # it 'returns json object' do
+    #   get :nearby_drivers, params: {lng: 58.382856, lat: 26.732627}
+    #   expect(response.content_type).to eq 'application/json'
+    # end
 
-    it 'returns non empty list of json objects' do
-      get :nearby_drivers, params: {lng: 58.382856, lat: 26.732627}
-      expect(response).to be_success
-      parsed_response = JSON.parse(response.body)
-      expect(parsed_response['drivers'].length).to be > 0
-    end
+    # it 'returns non empty list of json objects' do
+    #   get :nearby_drivers, params: {lng: 58.382856, lat: 26.732627}
+    #   expect(response).to be_success
+    #   parsed_response = JSON.parse(response.body)
+    #   expect(parsed_response['drivers'].length).to be > 0
+    # end
 
-    it 'returns list of objects containing expected keys' do
-      get :nearby_drivers, params: {lng: 58.382856, lat: 26.732627}
-      expect(response).to be_success
-      parsed_response = JSON.parse(response.body)
-      first_driver = parsed_response['drivers'][0]
-      expect(first_driver.keys).to contain_exactly('lng', 'ltd', 'car_info')
-    end
+    # it 'returns list of objects containing expected keys' do
+    #   get :nearby_drivers, params: {lng: 58.382856, lat: 26.732627}
+    #   expect(response).to be_success
+    #   parsed_response = JSON.parse(response.body)
+    #   first_driver = parsed_response['drivers'][0]
+    #   expect(first_driver.keys).to contain_exactly('lng', 'ltd', 'car_info')
+    # end
 
     it 'registers returns error message' do
       post :register_driver, params: {first_name: 'John', last_name: 'Doe'}
